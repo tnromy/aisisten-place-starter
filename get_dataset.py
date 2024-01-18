dc296bc44f48822cc94517b77c9f124a08b42e27
tnromy <tuanromy@gmail.com>
2024-01-18 19:22:40 +0700

    mendapatkan list review di tiap hotel dengan hit ke  API google maps 500 kali

--- get_dataset.py
+++ get_dataset.py
@@ -1,4 +1,5 @@
 import csv
+import re
 from google_maps import GoogleMaps
 class GetDataset:
     @staticmethod
@@ -14,8 +15,32 @@ class GetDataset:
                 place = [hotel_name, place_id]
                 places.append(place)
 
-        with open('places.csv', mode='a', newline='') as places_file:
+        with open('places.csv', mode='a', newline='', encoding='utf-8') as places_file:
             place_writer = csv.writer(places_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
             for place in places:
                 place_writer.writerow(place)
+
+    @staticmethod
+    def getPlacesReviews():
+        places_reviews = []
+
+        with open('hotel_id3.csv', newline='') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=';')
+            for row in csv_reader:
+                place_id = row[1].strip()
+
+                reviews = GoogleMaps.getPlaceReviews(place_id)
+                for review in reviews:
+                    text_review = re.sub(r'\r\n|\r|\n', ' ', review["text"])
+                    place_review = [place_id, text_review, review["rating"]]
+
+                    places_reviews.append(place_review)
+
+        with open('hotel_reviews3.csv', mode='a', newline='', encoding='utf-8') as places_file:
+            place_writer = csv.writer(places_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+            for place in places_reviews:
+                place_writer.writerow(place)
+
+GetDataset.getPlacesReviews()
