2798c09407b5500f029069634787782907d974ff
tnromy <tuanromy@gmail.com>
2024-01-18 18:35:00 +0700

    mendapatkan place_id untuk tiap nama hotel dengan looping dan hit ke API google maps 579 kali membuat method get detail reviews untuk 1 place_id

--- get_dataset.py
+++ get_dataset.py
@@ -1,16 +1,21 @@
 import csv
+from google_maps import GoogleMaps
+class GetDataset:
+    @staticmethod
+    def getPlacesId():
+        places = []
 
-places = []
+        with open('hotel.csv', newline='') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=';')
+            for row in csv_reader:
+                hotel_name = row[0].strip()
 
-with open('hotel.csv', newline='') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    for row in csv_reader:
-        hotel_name = row[0].strip()
-        place = [hotel_name, 'ada']
-        places.append(place)
+                place_id = GoogleMaps.find_place(hotel_name)
+                place = [hotel_name, place_id]
+                places.append(place)
 
-with open('places.csv', mode='a', newline='') as places_file:
-    place_writer = csv.writer(places_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        with open('places.csv', mode='a', newline='') as places_file:
+            place_writer = csv.writer(places_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    for place in places:
-        place_writer.writerow(place)
+            for place in places:
+                place_writer.writerow(place)
